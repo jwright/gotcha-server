15222affd50bd758a6efcc521ed742aff17177c0
Jamie Wright <jamie@brilliantfantastic.com>
2016-03-09 22:13:05 -0500

    Expand the scope so that the user and the team information can be read

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :slack, ENV["SLACK_CLIENT_ID"], ENV["SLACK_SECRET"], scope: "identify,bot"
+  provider :slack, ENV["SLACK_CLIENT_ID"], ENV["SLACK_SECRET"],
+    scope: "bot,identify,users:read,team:read"
 end
